8b69dc446950695cb2040f1eaf14510205bf1904
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-01-02 14:11:44 -0300

    Aula 01

---
+++ Curso07.APIsEMicrosservicos/01.MessageBrokers/Receive/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "minha-fila", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                Console.WriteLine(" [*] Aguardando mensagens.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] MENSAGEM RECEBIDA: {0}", message);
+                };
+                channel.BasicConsume(queue: "minha-fila", autoAck: true, consumer: consumer);
+
+                Console.WriteLine(" Tecle [enter] para sair.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Curso07.APIsEMicrosservicos/01.MessageBrokers/Sender/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "minha-fila", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                string message = "Saudações, eu vim do projeto Aula01.Send!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "", routingKey: "minha-fila", basicProperties: null, body: body);
+                Console.WriteLine(" [x] MENSAGEM ENVIADA: {0}", message);
+            }
+
+            Console.WriteLine(" Tecle [enter] para saiur.");
+            Console.ReadLine();
+        }
+    }
+}
