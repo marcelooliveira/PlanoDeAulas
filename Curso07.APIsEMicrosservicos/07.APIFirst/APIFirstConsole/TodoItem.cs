6170fd72e6c41a780dad84d18934a2d273b2c0e2
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-01-29 17:21:42 -0300

    .

--- Curso07.APIsEMicrosservicos/07.APIFirst/APIFirstConsole/TodoItem.cs
+++ Curso07.APIsEMicrosservicos/07.APIFirst/APIFirstConsole/TodoItem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace APIFirstConsole
+namespace APIFirstClient
 {
     public class TodoItem
     {
--- Curso07.APIsEMicrosservicos/07.APIFirst/APIFirstDemo/Controllers/TodoController.cs
+++ Curso07.APIsEMicrosservicos/07.APIFirst/APIFirstDemo/Controllers/TodoController.cs
@@ -13,59 +13,59 @@ namespace APIFirstDemo.Controllers
     {
         private readonly TodoService service = new TodoService();
 
-        /// <summary>
-        /// Cria uma nova tarefa
-        /// </summary>
-        /// <param name="item">Dados da tarefa</param>
-        /// <returns>tarefa criada</returns>
-        [HttpPost]
-        public ActionResult<TodoItem> Post([FromBody] TodoItem item)
-        {
-            return Ok(service.Create(item));
-        }
+        ///// <summary>
+        ///// Cria uma nova tarefa
+        ///// </summary>
+        ///// <param name="item">Dados da tarefa</param>
+        ///// <returns>tarefa criada</returns>
+        //[HttpPost]
+        //public ActionResult<TodoItem> Post([FromBody] TodoItem item)
+        //{
+        //    return Ok(service.Create(item));
+        //}
 
-        /// <summary>
-        /// Obtém uma tarefa por Id
-        /// </summary>
-        /// <param name="id">Id da tarefa</param>
-        /// <returns>Uma tarefa</returns>
-        [HttpGet("{id}")]
-        public ActionResult<TodoItem> Get(int id)
-        {
-            return Ok(service.Get(id));
-        }
+        ///// <summary>
+        ///// Obtém uma tarefa por Id
+        ///// </summary>
+        ///// <param name="id">Id da tarefa</param>
+        ///// <returns>Uma tarefa</returns>
+        //[HttpGet("{id}")]
+        //public ActionResult<TodoItem> Get(int id)
+        //{
+        //    return Ok(service.Get(id));
+        //}
 
-        /// <summary>
-        /// Cria ou modifica uma tarefa
-        /// </summary>
-        /// <param name="item">Dados da tarefa</param>
-        /// <returns>Dados da tarefa criada ou modificada</returns>
-        [HttpPut]
-        public ActionResult<TodoItem> Put([FromBody] TodoItem item)
-        {
-            return Ok(service.Save(item));
-        }
+        ///// <summary>
+        ///// Cria ou modifica uma tarefa
+        ///// </summary>
+        ///// <param name="item">Dados da tarefa</param>
+        ///// <returns>Dados da tarefa criada ou modificada</returns>
+        //[HttpPut]
+        //public ActionResult<TodoItem> Put([FromBody] TodoItem item)
+        //{
+        //    return Ok(service.Save(item));
+        //}
 
-        /// <summary>
-        /// Remove uma tarefa
-        /// </summary>
-        /// <param name="id">Id da Tarefa</param>
-        /// <returns></returns>
-        [HttpDelete("{id}")]
-        public ActionResult<TodoItem> Delete(int id)
-        {
-            return Ok(service.Delete(id));
-        }
+        ///// <summary>
+        ///// Remove uma tarefa
+        ///// </summary>
+        ///// <param name="id">Id da Tarefa</param>
+        ///// <returns></returns>
+        //[HttpDelete("{id}")]
+        //public ActionResult<TodoItem> Delete(int id)
+        //{
+        //    return Ok(service.Delete(id));
+        //}
 
-        /// <summary>
-        /// Obtém todas as tarefas
-        /// </summary>
-        /// <returns>Lista de tarefas</returns>
-        [HttpGet]
-        [Route("GetAll")]
-        public ActionResult<List<TodoItem>> GetAll()
-        {
-            return Ok(service.GetAll());
-        }
+        ///// <summary>
+        ///// Obtém todas as tarefas
+        ///// </summary>
+        ///// <returns>Lista de tarefas</returns>
+        //[HttpGet]
+        //[Route("GetAll")]
+        //public ActionResult<List<TodoItem>> GetAll()
+        //{
+        //    return Ok(service.GetAll());
+        //}
     }
 }
