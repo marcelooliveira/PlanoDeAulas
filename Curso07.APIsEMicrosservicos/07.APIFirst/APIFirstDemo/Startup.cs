e21fd3e79cec018b35531b8f4c69406c22835dce
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-01-31 00:12:20 -0300

    Swagger

--- Curso07.APIsEMicrosservicos/07.APIFirst/APIFirstDemo/Startup.cs
+++ Curso07.APIsEMicrosservicos/07.APIFirst/APIFirstDemo/Startup.cs
@@ -3,7 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 
 namespace APIFirstDemo
 {
@@ -23,6 +27,44 @@ namespace APIFirstDemo
         {
 
             services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                //Definir o documento OpenAPI
+                //que será criado pelo Swagger generator
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    //Versão do documento openApi
+                    Version = "v1",
+                    //Título da API
+                    Title = "ToDo API",
+                    //Descrição da API
+                    Description = "Uma Web API ASP.NET Core para gerenciamento de tarefas",
+                    //Termos de serviço
+                    TermsOfService = new Uri("https://example.com/termos"),
+                    //Contato
+                    Contact = new OpenApiContact
+                    {
+                        //Nome do contato
+                        Name = "Fulano de Tal",
+                        //Url do contato
+                        Url = new Uri("https://example.com/fulano")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        //Nome da licença
+                        Name = "Licença de exemplo",
+                        //Url da licença
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+
+                //arquivo xml
+                var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //incluir/injetar comentários xml
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
+                    xmlFileName));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +76,18 @@ namespace APIFirstDemo
             }
 
             app.UseRouting();
+
+            //registra o Swagger
+            app.UseSwagger();
+
+            //registra o Swagger com interface do usuário
+            app.UseSwaggerUI(options =>
+            {
+                //Adicionar endpoint do Swagger Json
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                //prefixo de rota para acessar o swagger-ui
+                options.RoutePrefix = string.Empty;
+            });
 
             app.UseAuthorization();
 
