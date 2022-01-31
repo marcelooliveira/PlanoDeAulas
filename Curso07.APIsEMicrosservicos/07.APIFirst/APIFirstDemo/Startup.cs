0c2512aec98b44b0e9748d9b9ec2e02b28808873
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-01-30 23:49:35 -0300

    .

--- Curso07.APIsEMicrosservicos/07.APIFirst/APIFirstDemo/Startup.cs
+++ Curso07.APIsEMicrosservicos/07.APIFirst/APIFirstDemo/Startup.cs
@@ -3,11 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using System;
 using System.Diagnostics;
-using System.IO;
-using System.Reflection;
 
 namespace APIFirstDemo
 {
@@ -27,31 +23,6 @@ namespace APIFirstDemo
         {
 
             services.AddControllers();
-
-            services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "ToDo API",
-                    Description = "Uma Web API ASP.NET Core para gerenciamento de tarefas",
-                    TermsOfService = new Uri("https://example.com/terms"),
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Example Contact",
-                        Url = new Uri("https://example.com/contact")
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "Example License",
-                        Url = new Uri("https://example.com/license")
-                    }
-                });
-
-                // using System.Reflection;
-                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,12 +31,6 @@ namespace APIFirstDemo
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(options =>
-                {
-                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-                    options.RoutePrefix = string.Empty;
-                });
             }
 
             app.UseRouting();
