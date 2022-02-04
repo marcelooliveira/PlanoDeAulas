e4ece6b152d56057b6e6613d1586227583ba84d3
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-02-04 18:19:28 -0300

    .

---
+++ Curso08.OqueE.Net4.5/01.Historico/Historico/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Historico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2002 - .NET Framework 1.0
+            //===========================
+            #region Common Language Runtime
+            //ILSPY
+            //ILSpy é o navegador e descompilador de assembly .NET de código aberto.
+
+            //https://www.microsoft.com/en-us/p/ilspy/9mxfbkfvsq13#activetab=pivot:overviewtab
+
+            //C#, F#, VB.NET -> COMPILADOR -> IL -> CLR -> CÓDIGO DE MÁQUINA
+            // < image url="$(ProjectDir)/il.png"/>
+            #endregion
+
+            // 2003 - .NET Framework 1.1
+            //===========================
+
+            // 2005 - .NET Framework 2.0
+            //===========================
+            //WinForms
+            //ASP.NET
+            //ADO.NET
+            //Partial classes
+            //Nullable types
+            //Generics
+
+            // 2006 - .NET Framework 3.0
+            //===========================
+            //WPF
+            //WCF
+            //WF
+            //Card Space
+
+            // 2007 - .NET Framework 3.5
+            //===========================
+            //LINQ
+            //Entity Framework
+
+            // 2010 - .NET Framework 4.0
+            //===========================
+            //Parallel LINQ
+            //Task Parallel Library
+
+            // 2012 - .NET Framework 4.5
+            //===========================
+            //.NET APIs for Store/UWP Apps
+            //Task-Based Async Model
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
