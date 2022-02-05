7b34c7638266d405bc622bfa42d522ef05e4ac22
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-02-05 14:47:18 -0300

    CodeFirst

---
+++ Curso08.OqueE.Net4.5/04.EntityFramework/CodeFirst/Model/Aluno.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstDemo.Model
+{
+    public class Aluno
+    {
+        public int ID { get; set; }
+        public string Sobrenome { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataMatricula { get; set; }
+
+        public virtual ICollection<Matricula> Matriculas { get; set; }
+    }
+
+    public enum Nota
+    {
+        A, B, C, D, F
+    }
+
+    public class Matricula
+    {
+        public int MatriculaID { get; set; }
+        public int CursoID { get; set; }
+        public int AlunoID { get; set; }
+        public Nota? Nota { get; set; }
+
+        public virtual Curso Curso { get; set; }
+        public virtual Aluno Student { get; set; }
+    }
+
+    public class Curso
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CursoID { get; set; }
+        public string Titulo { get; set; }
+        public int Creditos { get; set; }
+
+        public virtual ICollection<Matricula> Enrollments { get; set; }
+    }
+}
+
---
+++ Curso08.OqueE.Net4.5/04.EntityFramework/CodeFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using CodeFirstDemo.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criando e Inicializando banco de dados a partir do código C#...");
+            using (EscolaContext db = new EscolaContext())
+            {
+                new EscolaInitializer().InitializeDatabase(db);
+            }
+            Console.WriteLine("Concluído!");
+            Console.ReadKey();
+
+        }
+    }
+}
