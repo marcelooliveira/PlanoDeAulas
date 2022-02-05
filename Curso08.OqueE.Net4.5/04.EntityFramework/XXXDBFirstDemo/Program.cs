f23172e6f3534a94ff8ccf153ab2aa317030b0b3
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-02-05 12:30:49 -0300

    DBFirstDemo

--- Curso08.OqueE.Net4.5/04.EntityFramework/XXXDBFirstDemo/Program.cs
+++ Curso08.OqueE.Net4.5/04.EntityFramework/XXXDBFirstDemo/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DBFirstDemo
 {
@@ -10,6 +6,7 @@ namespace DBFirstDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
         }
     }
 }
