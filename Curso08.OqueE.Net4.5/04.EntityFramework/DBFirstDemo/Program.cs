b23d67ea73a4f292c99e7fa014e26bba583c9e92
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-02-05 14:18:40 -0300

    dabatase first

--- Curso08.OqueE.Net4.5/04.EntityFramework/DBFirstDemo/Program.cs
+++ Curso08.OqueE.Net4.5/04.EntityFramework/DBFirstDemo/Program.cs
@@ -10,6 +10,73 @@ namespace DBFirstDemo
     {
         static void Main(string[] args)
         {
+            using (var db = new LojaEntities())
+            {
+                //criando clientes e pedidos
+                Cliente cliente1 = new Cliente()
+                {
+                    Nome = "Smeagol",
+                    Email = "smeagol@gollum.com"
+                };
+                db.Clientes.Add(cliente1);
+                db.SaveChanges();
+
+                Cliente cliente2 = new Cliente()
+                {
+                    Nome = "Harry Potter",
+                    Email = "harry@potter.com"
+                };
+                db.Clientes.Add(cliente2);
+                db.SaveChanges();
+
+                Cliente cliente3 = new Cliente()
+                {
+                    Nome = "Thanos",
+                    Email = "thanos@inevitavel.com"
+                };
+                db.Clientes.Add(cliente3);
+                db.SaveChanges();
+
+                cliente1.Pedidoes.Add(new Pedido
+                {
+                    Item = "Precioso",
+                    Preco = 1000
+                });
+
+                cliente2.Pedidoes.Add(new Pedido
+                {
+                    Item = "Nimbus 2000",
+                    Preco = 10000
+                });
+
+                cliente3.Pedidoes.Add(new Pedido
+                {
+                    Item = "Joias do Infinito",
+                    Preco = 500000
+                });
+
+                db.SaveChanges();
+
+                //LINQ = LANGUAGE INTEGRATED QUERY
+                var query = from c in db.Clientes.Include("Pedidoes")
+                            select c;
+
+                foreach (var cliente in query)
+                {
+                    Console.WriteLine($"Cliente: {cliente.Nome}");
+                    Console.WriteLine("Pedidos:");
+                    Console.WriteLine("========");
+                    foreach (var p in cliente.Pedidoes)
+                    {
+                        Console.WriteLine($"Item: {p.Item}, Preço: {p.Preco}");
+                    }
+                }
+
+                Console.WriteLine("Pressione qualquer tecla...");
+                Console.ReadKey();
+            }
+
+
         }
     }
 }
