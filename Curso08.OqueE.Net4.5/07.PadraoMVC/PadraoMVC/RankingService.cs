c073ae9ea9af07cae50b870e4f4545e4873b0a44
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-02-12 16:20:08 -0300

    RankingService.cs

---
+++ Curso08.OqueE.Net4.5/07.PadraoMVC/PadraoMVC/RankingService.cs
@@ -0,0 +1,69 @@
+﻿using PadraoMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PadraoMVC
+{
+    public class RankingService
+    {
+        static readonly Dictionary<int, Score> scores
+            = new Dictionary<int, Score>();
+
+        public static int GetNextId()
+        {
+            int id = 0;
+            var last = scores.Values.LastOrDefault();
+            if (last != null)
+            {
+                id = last.Id;
+            }
+            id++;
+            return id;
+        }
+
+        public List<Score> GetAll()
+        {
+            return scores.Values.ToList();
+        }
+
+        public Score Get(int id)
+        {
+            return scores
+                .Where(i => i.Key == id)
+                .Select(i => i.Value)
+                .SingleOrDefault();
+        }
+
+        public Score Create(Score item)
+        {
+            int id = GetNextId();
+            var newItem = new Score(id, item.Avatar, item.PlayerName, item.Points);
+            scores.Add(id, newItem);
+            return newItem;
+        }
+
+        public Score Save(Score item)
+        {
+            if (scores.ContainsKey(item.Id))
+            {
+                scores[item.Id] = item;
+                return item;
+            }
+            else
+            {
+                return Create(item);
+            }
+        }
+
+        public Score Delete(int id)
+        {
+            Score item = null;
+            if (scores.ContainsKey(id))
+            {
+                item = scores[id];
+                scores.Remove(id);
+            }
+            return item;
+        }
+    }
+}
