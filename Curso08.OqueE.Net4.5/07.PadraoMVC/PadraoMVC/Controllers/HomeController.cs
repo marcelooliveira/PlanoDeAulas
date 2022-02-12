7dd69b66ea5ae2cedebf0f8bf3de119ddb5ad3d1
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-02-11 21:24:51 -0300

    07_PadraoMVC_03_Roteamento.mp4

--- Curso08.OqueE.Net4.5/07.PadraoMVC/PadraoMVC/Controllers/HomeController.cs
+++ Curso08.OqueE.Net4.5/07.PadraoMVC/PadraoMVC/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace PadraoMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(int id)
         {
             return View();
         }
