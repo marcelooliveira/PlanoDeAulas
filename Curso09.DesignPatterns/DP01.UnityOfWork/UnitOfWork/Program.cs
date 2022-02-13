82628e033d8749fea1b7b09553eb07ee92ef1c67
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-02-13 12:27:12 -0300

    GetAlunos

--- Curso09.DesignPatterns/DP01.UnityOfWork/UnitOfWork/Program.cs
+++ Curso09.DesignPatterns/DP01.UnityOfWork/UnitOfWork/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitOfWork.Entities;
 
 namespace UnitOfWork
 {
@@ -10,6 +11,29 @@ namespace UnitOfWork
     {
         static void Main(string[] args)
         {
+            ListaAlunos();
+        }
+
+        private static void ListaAlunos()
+        {
+            Console.WriteLine("Lista Alunos");
+            Console.WriteLine("============");
+            IEnumerable<Aluno> alunos = GetAlunos();
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine($"Aluno: id={aluno.ID}, nome={aluno.Nome} {aluno.Sobrenome}");
+            }
+        }
+
+        private static IEnumerable<Aluno> GetAlunos()
+        {
+            List<Aluno> alunos;
+            using (var context = new EscolaEntities())
+            {
+                alunos = context.Alunos.ToList();
+            }
+
+            return alunos;
         }
     }
 }
