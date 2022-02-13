96e620f8ffb2d6956f367631bf1ce263d45416fd
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-02-13 18:59:26 -0300

    strategy - início

--- Curso09.DesignPatterns/DP02.Strategy/Strategy/Program.cs
+++ Curso09.DesignPatterns/DP02.Strategy/Strategy/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Strategy
+namespace Strategy.RealWorld
 {
-    class Program
+    
+    /// <image src="$(ProjectDir)/drill2 .png" scale =".5"/>
+    /// <image src="$(ProjectDir)/drill3 .png" scale =".5"/>
+    /// <image src="$(ProjectDir)/strategy .png"/>
+
+    /// <summary>
+    /// Padrão de Design Estratégia
+    /// </summary>
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Dois contextos seguindo estratégias diferentes
+
+            SortedList alunos = new SortedList();
+
+            alunos.Add("Saulo");
+            alunos.Add("Janaelton");
+            alunos.Add("Luan");
+            alunos.Add("Adriano");
+            alunos.Add("Aline");
+
+            alunos.Sort();
+
+            // Aguarde o usuário
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// A classe de "contexto"
+    /// </summary>
+
+    public class SortedList
+    {
+        private List<string> list = new List<string>();
+
+        public void Add(string name)
+        {
+            list.Add(name);
+        }
+
+        public void Sort()
+        {
+            list.Sort();
+
+            // Percorrer a lista e exibir os resultados
+            foreach (string nome in list)
+            {
+                Console.WriteLine(" " + nome);
+            }
+            Console.WriteLine();
         }
     }
 }
